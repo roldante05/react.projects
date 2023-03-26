9e673a4a8eb0ce2426699a03e8aecb39b0ef8849
Dante Roldan <danteroldan72@gmail.com>
2023-03-25 23:00:03 -0300

    componentes

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,15 @@
+import Experience from "./components/Experience";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
        <Navbar />
+       <Hero/>
+       <Experience/>
+       <About/>
     </div>
   );
 }
---
+++ portfolio-react/src/components/About.js
@@ -0,0 +1,8 @@
+import { Box, Button, Typography, styled } from "@mui/material";
+// import Testimonial from "./Testimonial";
+
+const About = () => {
+  return <></>;
+};
+
+export default About;
