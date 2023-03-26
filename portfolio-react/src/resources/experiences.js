0667ed5fe7d75ebd50272a1b8e6dfecf9d8493bc
Dante Roldan <danteroldan72@gmail.com>
2023-03-25 22:59:21 -0300

    constantes

---
+++ portfolio-react/src/resources/experiences.js
@@ -0,0 +1,18 @@
+export const experiences = [
+    {
+        id: 0,
+        title: "Emprendimiento",
+        nombre: "Servicio Tecnico de PC",
+        fecha:"Marzo 2021 - Mayo 2022",
+        descripcion: ["Instalación de Software","Instalación de Programas","Mantenimiento de Software"],
+        imagen: "https://portfolio-dante.vercel.app/static/media/tecnico.234526f3871f6e33a707.png"
+    },
+    {
+        id: 1,
+        title: "FREELANCE",
+        nombre: "Programación Web",
+        fecha:"Mayo - Junio 2022",
+        descripcion: ["Desarrollo de paginas web", "Implementando tecnologías como HTML, CSS, Bootstrap y PHP"],
+        imagen: "https://portfolio-dante.vercel.app/static/media/web.d4039cd6bf2cf1c8026f.png"
+    },
+];
