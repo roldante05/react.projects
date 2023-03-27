53adec79cddbeb1d60acf27864ce1863855bc505
Dante Roldan <danteroldan72@gmail.com>
2023-03-27 18:49:53 -0300

    componentes

---
+++ portfolio-react/src/components/Icon.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+const Icon = ({name, src}) => {
+  return <Box sx={{display:"flex", flexDirection:"column", alignItems:"center", mb: 3}} >
+<img src={src} height="40px" width="40px" /> 
+<Typography variant="body1" sx={{color: "white", mt: 1, color:"gray"}}  >
+    {name}
+</Typography>
+  </Box>;
+};
+
+export default Icon;
