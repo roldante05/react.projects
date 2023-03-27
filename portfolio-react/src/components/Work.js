129fb887494bb54567536d8d672a32b47fe46a5c
Dante Roldan <danteroldan72@gmail.com>
2023-03-27 18:50:30 -0300

    cambios

--- portfolio-react/src/components/Work.js
+++ portfolio-react/src/components/Work.js
@@ -4,31 +4,41 @@ import reactImg from "../media/review-code.svg";
 import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 import { Box } from "@mui/system";
 
-const Work = () => {
+const Work = ({title, nombre, fecha, descripcion, img}) => {
   return (
     <Box
       sx={{
-        backgroundColor: "#000F2E",
+        
         padding: 3,
-        border: "1px solid #00C7FF",
-        borderRadius: "10px",
-        maxWidth: "315px",
-        postion: "relative",
-        mx: 3,
+        
+        width: "475px",
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center",
+        mx: "auto",
       }}
     >
+        <Box  component="img" sx={{ width:120, mt: 4, mb: 4}} src={img} />
+        <div> 
       <Typography variant="body1" sx={{ color: "white", mb: 2 }}>
-        Emprendimiento { " " }  Servicio Tecnico de PC { " " } Marzo 2021 - Mayo 2022
+      <h3>  { " " }</h3>
+        <h4> {nombre} </h4>
+        <h4> {fecha} </h4>
+    
         <ul>
-        <li>Instalación de Software</li>
-        <li>Instalación de Software</li>
-        <li>Instalación de Software</li>
+        {
+          descripcion.map((descripcion) => (
+        <li key={descripcion} >{descripcion}</li>
+            
+            ))
+          }
         </ul>
       </Typography>
       <Typography variant="body2">
-        <span style={{ color: "#33C7FF", fontWeight: "bold" }}>Cornelus</span>{" "}
-        <span style={{ color: "#7B89A8" }}>CEO - Jupiter</span>
+        <span style={{ color: "#33C7FF", fontWeight: "bold" }}> {title} </span>{" "}
+        <span style={{ color: "#7B89A8" }}></span>
       </Typography>
+         </div>
     </Box>
   );
 };
